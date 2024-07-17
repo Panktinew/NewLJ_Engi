f96073916dffef6f3022678fb2f45a554e5fb530
Panktinew <130337976+Panktinew@users.noreply.github.com>
2024-07-17 10:59:42 +0530

    Add files via upload

---
+++ firstproject/src/firstproject/FirstSimpleClass1.java
@@ -0,0 +1,16 @@
+package firstproject;
+
+public class FirstSimpleClass1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Let Us start to learn java!!");
+         int no_1=12,no_2=3;
+         float result;
+         result = no_1*no_2;
+         System.out.println(result);
+
+	}
+
+}
